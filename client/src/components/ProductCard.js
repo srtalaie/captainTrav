ff57f97c2a1af52125397e23a7d6772bb501fb56
srtalaie <srt4721@yahoo.com>
2019-09-17 16:58:18 -0700

    editing product card

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -3,23 +3,41 @@ import React from "react";
 function renderImage(imgSrc){
     return (
         <div>
-            <img src={imgSrc} width="20px" height="20px"></img>
+            <img src={imgSrc} width="20px" height="20px" style={imgStyle}></img>
         </div>
     )
 }
 
+const imgStyle = {
+    margin: "5px"
+}
+
+const imageDivStyle = {
+    display: "flex"
+}
+
+const nameDivStyle = {
+    display: "flex",
+    flexDirection: "column"
+}
+
 const ProductCard = ({ id, name, description, imgLinks, price, category, editButton, deleteButton }) => (
     <tr data-id={id} id={id}>
-        <td>{name}
+        <td>
+            <div style={nameDivStyle}>
+            {name}
             <button onClick={editButton}>Edit</button>
             <button onClick={deleteButton}>Delete</button>
+            </div>
         </td>
         <td>{description}</td>
         <td>{category}</td>
         <td>
+            <div style={imageDivStyle}>
             {imgLinks.map(img => {
                 return renderImage(img);
             })}
+            </div>
         </td>
         <td>${price}</td>
     </tr>
