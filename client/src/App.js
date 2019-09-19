ea6fc98785464cb890350988886b4d45d12ae605
srtalaie <srt4721@yahoo.com>
2019-09-19 11:43:40 -0700

    Added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ProductListing from './pages/Products/Products.js';
+import Home from './pages/Home/Home.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <ProductListing />
+      <Home />
     </div>
   );
 }
