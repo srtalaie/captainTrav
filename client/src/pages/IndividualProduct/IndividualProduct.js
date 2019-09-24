26c5907c518ba67d575f9208156abc8b193e6601
srtalaie <srt4721@yahoo.com>
2019-09-24 15:27:44 -0700

    individual product page state working

--- client/src/pages/IndividualProduct/IndividualProduct.js
+++ client/src/pages/IndividualProduct/IndividualProduct.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import API from '../../utils/API.js';
 import './style.css';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class IndividualProduct extends Component {
 
+    state = {
+        product: {},
+        id: this.props.location.pathname.split('/')[2]
+    }
     
+    componentDidMount(){
+        this.loadProduct();
+    }
+
+    loadProduct = () => {
+        API.getProduct(this.state.id)
+        .then(res => this.setState({ product: res.data }))
+        .catch(err => console.log(err))
+    }
+
     render(){
         return(
             <div>
--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import API from '../../utils/API.js';
 import './style.css';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class Products extends Component {
     
