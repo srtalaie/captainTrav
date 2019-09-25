9da6dc9fa9c379787b87dd700b7b5b237b908525
srtalaie <srt4721@yahoo.com>
2019-09-25 16:47:40 -0700

    Im crying

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -17,5 +17,5 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(proxy('/api/**', { target: 'http://localhost:3001' }));
-
+  app.use(proxy('/api/**', { target: 'https://safe-crag-59591.herokuapp.com/' }));
 };
