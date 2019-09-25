e2ad8bd74bf5090edb4545d22d5c636ce2dd858e
srtalaie <srt4721@yahoo.com>
2019-09-25 15:29:43 -0700

    better work for heroku

--- client/src/pages/ProductsBackEnd/ProductsBackEnd.js
+++ client/src/pages/ProductsBackEnd/ProductsBackEnd.js
@@ -42,7 +42,7 @@ class ProductsBackEnd extends Component {
             description: this.state.newProductDescription,
             category: this.state.newProductCategory,
             imgLink: this.state.newProductImgs.split(',').map(function(string){ return string.trim(); }),
-            price: this.state.newProductPrice
+            price: parseInt(this.state.newProductPrice)
         };
 
         API.addProduct(newProduct)
@@ -82,7 +82,7 @@ class ProductsBackEnd extends Component {
             description: this.state.newProductDescription,
             category: this.state.newProductCategory,
             imgLink: this.state.newProductImgs.split(',').map(function(string){ return string.trim(); }),
-            price: this.state.newProductPrice
+            price: parseInt(this.state.newProductPrice)
         };
 
         API.updateProduct(this.state.currentProductId, newEditedProduct)
