832c19246b5db145a9734e869587afb302f01848
srtalaie <srt4721@yahoo.com>
2019-09-25 15:02:47 -0700

    more testing for heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoLabEquipm
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 
 //routes
