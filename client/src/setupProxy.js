60646e72083d01f7d2ebf66d216c6b05b0c49884
srtalaie <srt4721@yahoo.com>
2019-09-25 16:41:25 -0700

    fix heroku

---
+++ client/src/setupProxy.js
@@ -0,0 +1,21 @@
+const proxy = require("http-proxy-middleware");
+
+// module.exports = app => {
+//   app.use(proxy("/api/*", { target: "http://localhost:3001/" }));
+// };
+
+// module.exports = function(app) {
+//   app.use('/api', proxy({
+//     target: 'https://safe-crag-59591.herokuapp.com/',
+//     changeOrigin: true,
+//     router: {
+//       // when request.headers.host == 'dev.localhost:3000',
+//       // override target 'http://www.example.org' to 'http://localhost:8000'
+//       'dev.localhost:3000': 'http://localhost:3001'
+//     }
+//   }));
+
+module.exports = function(app) {
+  app.use(proxy('/api/**', { target: 'http://localhost:3001' }));
+
+};
