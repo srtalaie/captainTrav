b40f8788a4e6192a1602e67d86d6dda123801e28
srtalaie <srt4721@yahoo.com>
2019-09-30 15:19:07 -0700

    remove http-proxy-middleware from root

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-const proxy = require("http-proxy-middleware");
 
 // Define middleware here
 app.use(cors());
