5b789da6d395c3ad6d59cd29646c5d891c6461e4
srtalaie <srt4721@yahoo.com>
2019-09-30 15:15:46 -0700

    revert 17

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ const proxy = require("http-proxy-middleware");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', proxy({
-  target: 'http://safe-crag-59591.herokuapp.com',
-  changeOrigin: true,
-  ws: true,
-  router: {
-    'localhost:3000': 'http://localhost:3001'
-  }
-}));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
