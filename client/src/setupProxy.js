9fee522f537759b6369dde42e2b3afb234b5e4b6
srtalaie <srt4721@yahoo.com>
2019-09-30 16:32:45 -0700

    basic starting point

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,9 +7,6 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use('/api', proxy({
     target: 'https://safe-crag-59591.herokuapp.com',
-    changeOrigin: true,
-    prependPath: true,
-    ws: true,
     router: {
       'localhost:3000': 'http://localhost:3001'
     }
