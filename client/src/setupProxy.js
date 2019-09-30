329cf3b7f00d259cbe7aaf38fa5caeaab637ec18
srtalaie <srt4721@yahoo.com>
2019-09-30 15:09:13 -0700

    heroku fix 16

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ const proxy = require("http-proxy-middleware");
 // };
 
 module.exports = function(app) {
-  app.use('**/api/**', proxy({
+  app.use('/api/', proxy({
     target: 'http://safe-crag-59591.herokuapp.com',
     changeOrigin: true,
     ws: true,
