6337a707c280a346e34a5fdcec2cb96f1aae9951
srtalaie <srt4721@yahoo.com>
2019-10-01 11:02:24 -0700

    heroku fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
-// module.exports = app => {
-//   app.use(proxy("/api/*", { target: "https://safe-crag-59591.herokuapp.com/", changeOrigin: true, }));
-// };
-
 module.exports = function(app) {
   app.use('/api/**', proxy({
     target: 'https://safe-crag-59591.herokuapp.com',
@@ -13,27 +9,4 @@ module.exports = function(app) {
       'localhost:3000': 'http://localhost:3001'
     }
   }));
-
-// module.exports = app => {
-//   app.use(proxy('/api/**', { 
-//     target: 'https://safe-crag-59591.herokuapp.com',
-//     changeOrigin: true,
-//     router: function(req) {
-//       console.log(req.headers.host);
-//       if(req.headers.host !== 'localhost:3000'){
-//         return 'https://safe-crag-59591.herokuapp.com'
-//       } else {
-//         return 'http://localhost:3001'
-//       }
-//     }
-//   }));
-
-// module.exports = (app) => {
-//   app.use(proxy('/api/**', { 
-//     target: 'https://safe-crag-59591.herokuapp.com',
-//     changeOrigin: true,
-//     router: {
-//       'localhost:3000/api/**': "http://localhost:3001/api/**"
-//     } 
-//   })); 
 };
