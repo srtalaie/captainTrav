851fc46433a3217f6aedf786af63f4de65078d0e
srtalaie <srt4721@yahoo.com>
2019-10-01 11:53:59 -0700

    back to start

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use('/api/**', proxy({
+  app.use('/api/*', proxy({
     target: 'https://safe-crag-59591.herokuapp.com',
     changeOrigin: true,
     logLevel: 'debug',
