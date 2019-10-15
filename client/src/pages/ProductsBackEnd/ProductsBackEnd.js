652d019a7676dac4d68d0dfcd89f5fbedff50ec7
srtalaie <srt4721@yahoo.com>
2019-10-14 17:14:24 -0700

    secured backend

--- client/src/pages/ProductsBackEnd/ProductsBackEnd.js
+++ client/src/pages/ProductsBackEnd/ProductsBackEnd.js
@@ -6,8 +6,8 @@ import ProductCard from '../../components/ProductCard.js';
 class ProductsBackEnd extends Component {
     state = {
         products: [],
-        username: 'root',
-        password: 'admin',
+        username: process.env.REACT_APP_USERNAME,
+        password: process.env.REACT_APP_PASSWORD,
         newProductName: '',
         newProductDescription: '',
         newProductCategory: '',
@@ -15,7 +15,11 @@ class ProductsBackEnd extends Component {
         newProductPrice: 0.00,
         productID: '',
         currentProductId: '',
-        beingEdited: false
+        beingEdited: false,
+        accountCheck: false,
+        hideLogin: false,
+        usernameInput: '',
+        usernamePass: ''
     }
 
     componentDidMount(){
@@ -120,65 +124,85 @@ class ProductsBackEnd extends Component {
         .catch(err => console.log(err))
     }
     
+    login = () => {
+        if(this.state.usernameInput === this.state.username && this.state.usernamePass === this.state.password){
+            this.setState({ accountCheck: true, hideLogin: true })
+        }
+    }
+
     render(){
         return(
             <div>
-                <div className="container">
-                    <div className="productCreate">
-                        <label for="newProductName">Name:</label>
-                        <input type="text" name="newProductName" value={this.state.newProductName} onChange={this.handleInput} className="productInputs"></input>
-                        <label for="newProductCategory">Category:</label>
-                        <select name="newProductCategory" value={this.state.newProductCategory} onChange={this.handleInput} className="productInputs">
-                            <option value="" selected disabled hidden>Choose Product Category</option>
-                            <option value="Andrology">Andrology</option>
-                            <option value="Microinjectors">Microinjectors</option>
-                            <option value="Incubators">Incubators</option>
-                            <option value="Centerfuges">Centerfuges</option>
-                        </select>
-                        <label for="newProductDescription">Description:</label>
-                        <textarea  name="newProductDescription" value={this.state.newProductDescription}onChange={this.handleInput} className="productInputs"></textarea>
-                        <label for="newProductImgs">Image URLs (comma separated list):</label>
-                        <input type="text" name="newProductImgs" value={this.state.newProductImgs} onChange={this.handleInput} className="productInputs"></input>
-                        <label for="newProductPrice">Price:</label>
-                        <input type="text" name="newProductPrice" value={this.state.newProductPrice} onChange={this.handleInput} className="productInputs"></input>
-                        {this.state.beingEdited ? (
-                            <div>
-                                <button onClick={this.submitEditedProduct}>Update Product</button>
-                                <button onClick={this.cancelEdit}>Cancel</button>
-                            </div>
-                        ): (
-                            <button onClick={this.createProduct}>Submit</button>
-                        )}
+                {this.state.hideLogin ? (
+                    <div>
+                        {this.state.accountCheck ? (
+                            <div className="container">
+                            <div className="productCreate">
+                                <label for="newProductName">Name:</label>
+                                <input type="text" name="newProductName" value={this.state.newProductName} onChange={this.handleInput} className="productInputs"></input>
+                                <label for="newProductCategory">Category:</label>
+                                <select name="newProductCategory" value={this.state.newProductCategory} onChange={this.handleInput} className="productInputs">
+                                    <option value="" selected disabled hidden>Choose Product Category</option>
+                                    <option value="Andrology">Andrology</option>
+                                    <option value="Microinjectors">Microinjectors</option>
+                                    <option value="Incubators">Incubators</option>
+                                    <option value="Centerfuges">Centerfuges</option>
+                                </select>
+                                <label for="newProductDescription">Description:</label>
+                                <textarea  name="newProductDescription" value={this.state.newProductDescription}onChange={this.handleInput} className="productInputs"></textarea>
+                                <label for="newProductImgs">Image URLs (comma separated list):</label>
+                                <input type="text" name="newProductImgs" value={this.state.newProductImgs} onChange={this.handleInput} className="productInputs"></input>
+                                <label for="newProductPrice">Price:</label>
+                                <input type="text" name="newProductPrice" value={this.state.newProductPrice} onChange={this.handleInput} className="productInputs"></input>
+                                {this.state.beingEdited ? (
+                                    <div>
+                                        <button onClick={this.submitEditedProduct}>Update Product</button>
+                                        <button onClick={this.cancelEdit}>Cancel</button>
+                                    </div>
+                            ): (
+                                <button onClick={this.createProduct}>Submit</button>
+                            )}
+                        </div>
+    
+                        <table className="productTable">
+                            <thead>
+                                <tr>
+                                    <td>Product Name</td>
+                                    <td>Product Description</td>
+                                    <td>Product Category</td>
+                                    <td>Product Images</td>
+                                    <td>Product Price</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.products.map(product => {
+                                    return(
+                                        <ProductCard
+                                            id = {product._id}
+                                            name = {product.name}
+                                            description = {product.description}
+                                            category = {product.category}
+                                            imgLinks = {product.imgLink}
+                                            price = {product.price}
+                                            editButton = {() => this.editProductSetUp(product)}
+                                            deleteButton = {() => this.deleteProduct(product._id)}
+                                        />    
+                                    )
+                                })}
+                            </tbody>
+                        </table>
                     </div>
-
-                    <table className="productTable">
-                        <thead>
-                            <tr>
-                                <td>Product Name</td>
-                                <td>Product Description</td>
-                                <td>Product Category</td>
-                                <td>Product Images</td>
-                                <td>Product Price</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.products.map(product => {
-                                return(
-                                    <ProductCard
-                                        id = {product._id}
-                                        name = {product.name}
-                                        description = {product.description}
-                                        category = {product.category}
-                                        imgLinks = {product.imgLink}
-                                        price = {product.price}
-                                        editButton = {() => this.editProductSetUp(product)}
-                                        deleteButton = {() => this.deleteProduct(product._id)}
-                                    />    
-                                )
-                            })}
-                        </tbody>
-                    </table>
+                    ):(
+                        <h1>Sorry incorrect password or username</h1>
+                    )}
                 </div>
+                ):(
+                    <div>
+                        <h3>Login:</h3><input type='text' name='usernameInput' value={this.state.usernameInput} onChange={this.handleInput}></input>
+                        <h3>Password:</h3><input type='text' name='usernamePass' value={this.state.usernamePass} onChange={this.handleInput}></input>
+                        <button onClick={this.login}>Login</button>
+                    </div>
+                )}
             </div>
         );
     }
